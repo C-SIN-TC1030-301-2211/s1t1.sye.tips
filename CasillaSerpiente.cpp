31182dca85f209bfd1e97c69505873cd8b6d5b00
Ricardo Quintero <55771796+rquinteroTecSin@users.noreply.github.com>
2022-05-27 23:58:13 +0000

    CSerpiente...

--- CasillaSerpiente.cpp
+++ CasillaSerpiente.cpp
@@ -2,11 +2,13 @@
 #include "CCasilla.hpp"
 #include "Ctesconf.hpp"
 
+// Inicializa la Casilla Serpiente en una posicion valida, con su premio/castigo y tipo
 CasillaSerpiente::CasillaSerpiente()
 {
     
 }
 
+// Inicializa la Casilla Serpiente como Casilla Normal configurada como Serpiente y agrega el premio/castigo
 CasillaSerpiente::CasillaSerpiente(int numero):CCasilla{numero, "S"}
 {
     

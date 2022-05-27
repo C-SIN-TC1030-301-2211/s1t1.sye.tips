33ce7a69951060590ef8a1ee9de756ec6e29b1d2
Ricardo Quintero <55771796+rquinteroTecSin@users.noreply.github.com>
2022-05-27 23:56:02 +0000

    CEscalera...

--- CasillaEscalera.cpp
+++ CasillaEscalera.cpp
@@ -1,11 +1,13 @@
 #include "CasillaEscalera.hpp"
 #include "Ctesconf.hpp"
 
+//Inicializa la Casilla Escalera configurada con su premio/castigo y su tipo
 CasillaEscalera::CasillaEscalera()
 {
     
 }
 
+//Inicializa la Casilla Escalera como Casilla Normal tipo Escalera y agregando el premio/castigo
 CasillaEscalera::CasillaEscalera(int numero):CCasilla{numero, "L"}
 {
     

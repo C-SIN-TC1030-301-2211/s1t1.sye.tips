ac8fd1ff00d04673f133040af4bc25746549df5e
Ricardo Quintero <55771796+rquinteroTecSin@users.noreply.github.com>
2022-05-27 23:52:43 +0000

    CCasilla...

--- CCasilla.cpp
+++ CCasilla.cpp
@@ -1,29 +1,37 @@
 #include "CCasilla.hpp"
 #include "Ctesconf.hpp"
 
+// Inicializa la casilla con un valor de 1 y como normal
 CCasilla::CCasilla()
 {
 }
+
+
+//Inicializa la casilla con un valor y tipo enviados como parametro
 CCasilla::CCasilla(int val, std::string tipo)
 {
     
 }
 
+//Consulta la casilla actual
 int CCasilla::getNumeroCasilla()
 {
     
 }
 
+//Consulta el tipo de la casilla actual
 std::string CCasilla::getTipo()
 {
     
 }
 
+//Determina la casilla siguiente dependiendo del premio/castigo. Nunca debería pasar del maximo de casillas
 int CCasilla::getSiguienteCasilla()
 {
     
 }
 
+//Muestra en consola el numero de casilla, su tipo y el premio/castigo correspondiente
 void CCasilla::print()
 {
     

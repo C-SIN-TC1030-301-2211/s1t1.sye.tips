ff23419bf46c349b16b65b218ab2e9c599832f8d
Ricardo Quintero <55771796+rquinteroTecSin@users.noreply.github.com>
2022-05-27 23:54:24 +0000

    CNormal...

--- CasillaNormal.cpp
+++ CasillaNormal.cpp
@@ -1,9 +1,12 @@
 #include "CasillaNormal.hpp"
 #include "CCasilla.hpp"
+
+//Inicializa la casilla Normal Igual que la casilla base
 CasillaNormal::CasillaNormal() : CCasilla{}
 {
 }
 
+//Inicializa la casilla Normal como casilla base indicando su posicion en el Tablero
 CasillaNormal::CasillaNormal(int numero):CCasilla{}
 {
 }
